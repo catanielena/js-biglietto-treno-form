3775b0242f0c1e6e63823681c03847aab10ac3f1
catanielena <catani.elena1@gmail.com>
2021-08-31 14:34:50 +0200

    Refact: toFixed; Add: data-control

--- js/main.js
+++ js/main.js
@@ -8,39 +8,46 @@ btnGenera.addEventListener("click",
         var km = parseInt(document.getElementById("km").value);
         var ageRange = document.getElementById("age-range").value;
         console.log(nome, km, ageRange);
-        // carrozza
-        var carriage = Math.floor(Math.random() * 15) + 1;
-        // codice cp
-        var cpCode = "";
-        for (var i=0; i<5; i++) {
-            cpCode += (Math.floor(Math.random() *10) +1);
+        if ( nome !== "" && !isNaN(km) && km > 0 && ageRange !== "") {
+            // carrozza
+            var carriage = Math.floor(Math.random() * 15) + 1;
+            // codice cp
+            var cpCode = "";
+            for (var i=0; i<5; i++) {
+                cpCode += (Math.floor(Math.random() *10));
+            }
+    
+            // prezzo-biglietto e condizioni di offerta
+            var ticketCost = km * 0.21;
+            var offer = "Standard";
+            if (ageRange == "minorenne") {
+                ticketCost -= ticketCost * 0.2;
+                offer = "Sconto minorenne";
+            } else if (ageRange == "over65") {
+                ticketCost -= ticketCost * 0.4;
+                offer = "Sconto Silver";
+            };
+            var ticketCost = ticketCost.toFixed(2);
+            // Print data
+            document.getElementById("passenger").innerHTML = nome;
+            document.getElementById("offer").innerHTML = offer;
+            document.getElementById("ticket-cost").innerHTML = ticketCost;
+            document.getElementById("carriage").innerHTML = carriage;
+            document.getElementById("cp-code").innerHTML = cpCode;
+            document.getElementById("ticket-cost").innerHTML = ticketCost;
+            document.getElementById("ticket-data").classList.add("display--block");
+        } else {
+            alert("Errore");
         }
-        // prezzo-biglietto e condizioni di offerta
-        var ticketCost = km * 0.21.toFixed(2);
-        var offer = "Standard";
-        if (ageRange == "minorenne") {
-            ticketCost -= ticketCost * 0.2.toFixed(2);
-            offer = "Under 18";
-        } else if (ageRange == "over65") {
-            ticketCost -= ticketCost * 0.4.toFixed(2);
-            offer = "Silver";
-        };
-        // Print data
-        document.getElementById("passenger").innerHTML = nome;
-        document.getElementById("offer").innerHTML = offer;
-        document.getElementById("ticket-cost").innerHTML = ticketCost;
-        document.getElementById("carriage").innerHTML = carriage;
-        document.getElementById("cp-code").innerHTML = cpCode;
-        document.getElementById("ticket-cost").innerHTML = ticketCost;
-        document.getElementById("ticket-data").classList.add("display--block");
     }
 );
 
+// btnAnnulla
 btnAnnulla.addEventListener("click",
     function() {
         document.getElementById("ticket-data").classList.remove("display--block");
         document.getElementById("nome-cognome").value = "";
         document.getElementById("km").value = "";
-        document.getElementById("age-range").value = "";
+        document.getElementById("age-range").selectedIndex = "0";
     }
-);
+); 
